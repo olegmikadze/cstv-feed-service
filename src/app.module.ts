256bc733b467c11cb297e8867ecc2994369eb27e
Oleh Mikadze <oleh.mikadze@folderly.com>
2024-06-13 18:56:59 +0200

    clear main module from useless files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController as AddController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FeedModule } from './feed/feed.module';
 
 @Module({
   imports: [MongooseModule.forRoot('ADD_LINK'), FeedModule],
-  controllers: [AddController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
