0d9c8a084c2e2a4ca429cf2f75546ec38112cdf0
Oleh Mikadze <oleh.mikadze@folderly.com>
2024-06-19 13:19:56 +0200

    get incidents

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,18 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Get } from '@nestjs/common';
 import { FeedService } from './feed.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { ClientProxy, MessagePattern } from '@nestjs/microservices';
 
 @Controller('feed')
 export class FeedController {
-  constructor(private readonly feedService: FeedService) {}
+  constructor(
+    private readonly feedService: FeedService,
+    @Inject('FEED_SERVICE') private readonly client: ClientProxy,
+  ) {}
 
   @MessagePattern({ cmd: 'hello' })
   async getHello(): Promise<string> {
     return await this.feedService.getHello();
   }
 
-  @MessagePattern({ cmd: 'incidents' })
-  async getIncidents() {
-    await this.feedService.getIncidents();
+  @Get('/')
+  async testGetIncidents() {
+    const pattern = { cmd: 'sum' };
+    const data = [1, 2, 3, 4, 5];
+    return await this.client.send<number>(pattern, data);
+  }
+
+  @MessagePattern({ cmd: 'get-incidents' })
+  async getIncidents(): Promise<any> {
+    return await this.feedService.getIncidents();
   }
 }
